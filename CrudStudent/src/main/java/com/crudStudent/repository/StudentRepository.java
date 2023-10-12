065d044b393e64fdcc8283d2528a54e74eff518e
Nitesh Kumar Pal <101392580+niteshpalcode@users.noreply.github.com>
2023-10-12 17:59:38 +0530

    Delete CrudStudent directory

--- CrudStudent/src/main/java/com/crudStudent/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crudStudent.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.crudStudent.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
